eacf3dc4a75289dd76a73ba3bfce441c8670fa51
GabyAng2002 <Gavyang.2002@proton.me>
2025-02-15 09:01:02 -0600

    Loading Component To Start

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,8 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { InfoModalComponent } from '../info-modal/info-modal.component';
 
 @Component({
@@ -17,11 +19,16 @@ export class LoginPage {
   password: string = '';
   isValid: boolean = false;
 
-  constructor(private modalController: ModalController) {}
+  constructor(private modalController: ModalController, private router: Router, private loadingController: LoadingController) { }
+
+  acces = [
+    { username: 'admin', password: '1234' },
+    { username: 'gaby', password: '1234' }
+  ];
 
   validarCampos() {
-    this.username = this.username.replace(/\s+/g, '').toLowerCase();
-    this.password = this.password.replace(/\s+/g, '');
+    this.username = this.username.replace(/\s+/g, '').toLowerCase(); //Se encarga de remplazar mayúsculas por minúsculas y no aceptar espacios
+    this.password = this.password.replace(/\s+/g, ''); //Igualmente no acepta espacios
 
     this.isValid = this.username.length > 0 && this.password.length > 0;
   }
@@ -35,5 +42,30 @@ export class LoginPage {
       },
     });
     return await modal.present();
+  }
+ //Author: LAURA GABRIELA ANGUIANO PINEDA
+  acceder() {
+    const usuarioEncontrado = this.acces.find(user => 
+      user.username === this.username && user.password === this.password
+    );
+  
+    if (usuarioEncontrado) {
+      this.presentLoading('Entrando', () => {
+        this.router.navigate(['/home']); 
+      });
+    } else {
+      alert('Datos incorrectos');
+    }
+  }
+ //Author: LAURA GABRIELA ANGUIANO PINEDA
+  // Función reutilizable para mostrar un Loading
+  async presentLoading(mensaje: string, callback: Function) {
+    const loading = await this.loadingController.create({
+      message: mensaje,
+      duration: 3000 // 3 segundos
+    });
+    await loading.present();
+    await loading.onDidDismiss();
+    callback(); // Ejecuta la función después del loading
   }
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
+  standalone: true,
   imports: [CommonModule, IonicModule, ReactiveFormsModule]
 })
 export class RegisterPage implements OnInit {
   registerForm: FormGroup;
+  users: any[] = [];
 
-  users: any[] = []; 
-
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router, private loadingController: LoadingController) {
     this.registerForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      fullName: ['', [Validators.required]],
-      user: ['', [Validators.required, Validators.pattern(/^\S*$/)]], // Sin espacios
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', [Validators.required]],
-      birthDate: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]], // Validar que ingrese este campo y que sea un correo válido
+      fullName: ['', [Validators.required]], // Validar que ingrese este campo
+      user: ['', [Validators.required, Validators.pattern(/^\S*$/)]], // Validar que no tenga espacios
+      password: ['', [Validators.required, Validators.minLength(6)]], // Validar que tenga al menos 6 carácteres
+      confirmPassword: ['', [Validators.required]], // Validar que ingrese este campo
+      birthDate: ['', [Validators.required]], // Validar que ingrese este campo
     }, { validators: this.matchPasswords });
   }
 
@@ -29,32 +31,68 @@ export class RegisterPage implements OnInit {
   }
 
   matchPasswords(group: FormGroup) {
-    const password = group.get('password')?.value;
-    const confirmPassword = group.get('confirmPassword')?.value;
-    return password === confirmPassword ? null : { passwordsMismatch: true };
+    const password = group.get('password')?.value; // Obtenemos el password del campo
+    const confirmPassword = group.get('confirmPassword')?.value; // Obtenemos el segundo password
+    return password === confirmPassword ? null : { passwordsMismatch: true }; // Comparamos ambos que sean exactamente iguaes
   }
 
   toUpperCase() {
-    const fullName = this.registerForm.get('fullName');
+    const fullName = this.registerForm.get('fullName'); // Obtenemos el nombre del campo
     if (fullName) {
-      fullName.setValue(fullName.value.toUpperCase(), { emitEvent: false });
+      fullName.setValue(fullName.value.toUpperCase(), { emitEvent: false }); // Lo converttimos a mayúsculas
     }
   }
 
   get passwordsDoNotMatch() {
     return this.registerForm.hasError('passwordsMismatch') &&
-           this.registerForm.get('confirmPassword')?.touched;
+      this.registerForm.get('confirmPassword')?.touched;
   }
 
   get email() { return this.registerForm.get('email'); }
   get user() { return this.registerForm.get('user'); }
-
-  register() {
+//Author: LAURA GABRIELA ANGUIANO PINEDA
+  async register() {
     if (this.registerForm.valid) {
-      this.users.push(this.registerForm.value); 
-      console.log('Datos del usuario:', this.registerForm.value);
-      alert('Registro exitoso');
-      this.registerForm.reset();
+      
+      const loading = await this.presentLoading('Registrando...');
+
+      setTimeout(async () => {
+        this.users.push(this.registerForm.value);
+        console.log('Datos del usuario:', this.registerForm.value);
+        alert('Registro exitoso');
+
+        this.registerForm.reset();
+        await loading.dismiss(); // Cerrar  spinner
+
+        this.router.navigate(['/login']);
+      }, 3000); 
+      // Simulación de carga de 3 segundos
     }
   }
+  //Author: LAURA GABRIELA ANGUIANO PINEDA
+  async presentLoading(message: string) {
+    const loading = await this.loadingController.create({
+      message: message,
+      spinner: 'crescent', // Se usa el spinner de carga predeterminado
+      backdropDismiss: false,
+      duration: 0 // Para que no desaparezca instantaneamente
+    });
+
+    // Añadir HTML personalizado dentro del loading
+    const loadingElement = await loading.present();
+
+    // Modificar instancia
+    const content = document.querySelector('.loading-content');
+    if (content) {
+      content.innerHTML = `
+        <img src="/assets/Logo.png" class="loading-image" alt="Logo-UTEQ">
+        <div class="loading-text" style="display: flex; justify-content: center; text-align: center; 15px; color: #fff;">
+            ${message}
+        </div>
+      `;
+    }
+
+    return loading;
+  }
+
 }
